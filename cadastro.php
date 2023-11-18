33986df80bcac7be92632b3edb55d4e3a220c832
xandjr <102760988+xandjr@users.noreply.github.com>
2023-11-18 18:42:12 -0300

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,30 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "recitech";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn -> connect_error) {
+    die("Conexão falhou: " . $conn -> connect_error);
+}
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$endereco = $_POST['endereco'];
+$telefone = $_POST['telefone'];
+$senha = $_POST['senha'];
+$senha2 = $_POST['senha2'];
+
+$sql = "INSERT INTO cadastros (nome, email, endereco, telefone, senha, senha2) VALUES ('$nome', '$email', '$endereco', '$telefone', '$senha', '$senha2')";
+
+if ($conn -> query($sql) === TRUE) {
+    echo "Cadastro realizado!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn -> error;
+}
+
+$conn -> close();
+?>
