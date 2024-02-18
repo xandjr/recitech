9ec717738c17fb9e2ccc3c968168293b39655fe1
xandjr <102760988+xandjr@users.noreply.github.com>
2024-02-18 20:08:23 -0300

    Add files via upload

--- bd/recitech.sql
+++ bd/recitech.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 15-Fev-2024 às 18:34
+-- Tempo de geração: 19/02/2024 às 00:03
 -- Versão do servidor: 10.4.28-MariaDB
--- versão do PHP: 8.0.28
+-- Versão do PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cadastros`
+-- Estrutura para tabela `cadastros`
 --
 
 CREATE TABLE `cadastros` (
@@ -34,21 +34,69 @@ CREATE TABLE `cadastros` (
   `endereco` varchar(255) NOT NULL,
   `telefone` varchar(20) NOT NULL,
   `foto` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
+  `senha` varchar(255) NOT NULL,
+  `metas_concluidas` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `conquistas`
+--
+
+CREATE TABLE `conquistas` (
+  `id` int(20) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_meta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `metas`
+--
+
+CREATE TABLE `metas` (
+  `id` int(20) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `premio` varchar(255) NOT NULL,
+  `pontuacao` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `metas`
+--
+
+INSERT INTO `metas` (`id`, `titulo`, `descricao`, `premio`, `pontuacao`) VALUES
+(1, 'Amor Positivo', 'Descarte 10kg de lixo eletrônico', 'Amante da Natureza', 0);
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `cadastros`
+-- Índices de tabela `cadastros`
 --
 ALTER TABLE `cadastros`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- Índices de tabela `conquistas`
+--
+ALTER TABLE `conquistas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_meta` (`id_meta`);
+
+--
+-- Índices de tabela `metas`
+--
+ALTER TABLE `metas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
@@ -56,6 +104,29 @@ ALTER TABLE `cadastros`
 --
 ALTER TABLE `cadastros`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `conquistas`
+--
+ALTER TABLE `conquistas`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `metas`
+--
+ALTER TABLE `metas`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `conquistas`
+--
+ALTER TABLE `conquistas`
+  ADD CONSTRAINT `conquistas_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `cadastros` (`id`),
+  ADD CONSTRAINT `conquistas_ibfk_2` FOREIGN KEY (`id_meta`) REFERENCES `metas` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
